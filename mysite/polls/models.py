c20a6d1f445930f75856bdc6c6cafa1d3bef1aea
TylerGwynn <tylergwynn23@gmail.com>
2021-01-25 15:00:05 -0500

    added choices to admin, enforced unique q and c

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200, unique=True)
     pub_date = models.DateTimeField('date published')
     def __str__ (self):
         return self.question_text
@@ -17,6 +17,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    class Meta: 
+        unique_together = ['question', 'choice_text']
 
     def __str__(self):
         return self.choice_text
